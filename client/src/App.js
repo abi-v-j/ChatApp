632cff03b8ecc0e15eb7f318cdb7cc7b47025f6c
abi-v-j <abijoy611@gmail.com>
2023-12-20 18:13:43 +0530

    new

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Box, Button, Container, FormControl, Input, InputLabel, TextField, Typography } from '@mui/material';
+import { io } from 'socket.io-client'
+
 
 const App = () => {
+  const [socket, setSocket] = useState(null)
+  const [message, setMessage] = useState('')
+  const [chat, setChat] = useState([])
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit('send-message', { message })
+    setMessage('')
+
+
+  }
+
+  useEffect(() => {
+    setSocket(io("http://localhost:7000"))
+
+  }, [])
+
+  useEffect(() => {
+    if (!socket) return
+
+
+    socket.on('message-from-server', (data) => {
+      setChat((prevChat) => [...prevChat, data.message]);
+
+    })
+
+
+
+
+  }, [socket])
+
+
+
   return (
     <div>
-      Hello socket
+      <Container>
+        <Box sx={{marginBottom:5}}>
+        {
+          chat.map((msg, key) => (
+
+            <Typography key={key}>{msg}</Typography>
+          ))
+        }
+        </Box>
+      <Box component='form' onSubmit={handleSubmit}>
+      <FormControl fullWidth sx={{ m: 1 }} variant="standard">
+          <InputLabel htmlFor="standard-adornment-amount">Amount</InputLabel>
+          <Input
+            id="standard-adornment-amount"
+          />
+        </FormControl>
+        <Button variant="contained" type='submit'>Send</Button>
+      </Box>
+      </Container>
     </div>
   )
 }
--- server/Server.js
+++ server/Server.js
@@ -1,30 +1,46 @@
 import express from 'express'
-import http  from 'http'
+import http from 'http'
 import { Server } from 'socket.io'
 const app = express()
 const PORT = 7000
 
 const httpServer = http.createServer(app)
-const io = new Server(httpServer)
-
-import path from 'path'
-import { fileURLToPath } from 'url'
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-
-
-
-app.get('',(req,res) => {
- //  res.json({data:'hello world'})
- res.sendFile(path.join(__dirname, 'index.html'));
-
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:3000",
+    }
 })
+
+// import path from 'path'
+// import { fileURLToPath } from 'url'
+// const __filename = fileURLToPath(import.meta.url)
+// const __dirname = path.dirname(__filename)
+
+
+
+
+// app.get('',(req,res) => {
+//  //  res.json({data:'hello world'})
+//  res.sendFile(path.join(__dirname, 'index.html'));
+
+// })
 
 io.on('connection', (socket) => {
-    console.log('connection is ready');
+    // console.log('connection is ready');
+    socket.on('send-message', (data) => {
+        socket.broadcast.emit("message-from-server", data)
+    })
+    socket.on('disconnect', (socket) => {
+        console.log('User is left');
+        // socket.on('send-message', (data) => {
+        //     socket.emit("message-from-server", data)
+        // })                         
+    })
 })
 
+
+
+
 httpServer.listen(PORT, () => {
-   console.log('server is running ');
+    console.log('server is running ');
 })
