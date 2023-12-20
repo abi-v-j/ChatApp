e9581965726270ae89ff0aaa1720217796f16166
abi-v-j <abijoy611@gmail.com>
2023-12-20 14:34:37 +0530

    new

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      Hello socket
+    </div>
+  )
+}
+
+export default App
--- server/Server.js
+++ server/Server.js
@@ -7,10 +7,18 @@ const PORT = 7000
 const httpServer = http.createServer(app)
 const io = new Server(httpServer)
 
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+
 
 
 app.get('',(req,res) => {
-   res.json({data:'hello world'})
+ //  res.json({data:'hello world'})
+ res.sendFile(path.join(__dirname, 'index.html'));
+
 })
 
 io.on('connection', (socket) => {
